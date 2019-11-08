83128446f7cec23fd21c9a739b310f88a3a9c769
Carlos "Kurolox" Gomez <contact@carlosg.dev>
2019-11-08 18:57:50 +0100

    Added final status of the project at finish

--- src/problem_4.py
+++ src/problem_4.py
@@ -20,15 +20,18 @@ def add_to_output(line: str) -> None:
         problem_output.write(line + "\n")
 
 
-def is_inner(matrix, size, x, y):
-    if x == 0 or y == 0 or x == size[1] - 1 or y == size[0] - 1:
+def calc_endpoint(size, ray_origin, ray_direction):
+    while True:
+        ray_origin = np.add(ray_direction, ray_origin)
+
+        if ray_origin[0] <= 0 or ray_origin[1] <= 0 or ray_origin[0] >= size[1] - 1 or ray_origin[1] >= size[0] - 1:
+            return ray_origin
+
+
+def is_inside(size, point):
+
+    if point[0] < 0 or point[1] < 0 or point[0] > size[1] - 1 or point[1] > size[0] - 1:
         return False
-    else:
-        checks = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-        for diff in checks:
-            current_coords = np.add([x, y], diff)
-            if matrix[current_coords[1]][current_coords[0]] != 1:
-                return False
     return True
 
 
@@ -53,4 +56,47 @@ if __name__ == "__main__":
 
                 points = [*ray_origin]
 
-                add_to_output(f"{' '.join([str(a) for a in points])}")
+                mess_point_interval = [i/2 for i in ray_direction]
+                # Continue Here
+
+                coord_a = ray_origin
+                coord_b = calc_endpoint(size, ray_origin, ray_direction)
+
+                calc_range = np.linspace(0, 1, 1001)
+                solutions = [ray_origin]
+
+                for i, ratio in enumerate(calc_range):
+                    distance_x = ((coord_b[0] - coord_a[0])
+                                  * ratio) + coord_a[0]
+                    distance_y = ((coord_b[1] - coord_a[1])
+                                  * ratio) + coord_a[1]
+
+                    endpoint = [int(round(distance_x)), int(round(distance_y))]
+
+                    
+                    previous_endpoint = solutions[-1]
+
+                    if endpoint[0] != previous_endpoint[0] and endpoint[1] != previous_endpoint[1]:
+
+                        missing_point_1 = [endpoint[0], previous_endpoint[1]]
+                        missing_point_2 = [previous_endpoint[0], endpoint[1]]
+                        
+                        # Skipped midpoints, calc intervals
+                        if abs(ray_direction[0]) >= abs(ray_direction[1]):
+                            #if missing_point_1[0] > missing_point_2[0]:
+                            solutions.append(missing_point_1)
+                            solutions.append(missing_point_2)
+
+                        else:
+                            solutions.append(missing_point_2)
+                            solutions.append(missing_point_1)
+
+                    if endpoint not in solutions:
+                        solutions.append(endpoint)
+
+                solutions = [
+                    solution for solution in solutions if is_inside(size, solution)]
+                # End here
+
+                add_to_output(
+                    f"{' '.join([str(' '.join([str(i) for i in a])) for a in solutions])}")
